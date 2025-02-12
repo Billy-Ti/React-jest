a58db9ba213b53453361ad30aa49e28f91de51ae
Billy-Ti <show81728@gmail.com>
2025-02-12 12:23:37 +0800

    [fix] Fixed the issue that TypeScript cannot read jest types 1. Add types to tsconfig.json.

--- src/components/test/Button.test.tsx
+++ src/components/test/Button.test.tsx
@@ -3,11 +3,12 @@ import Button from "../Button";
 
 test("renders button with correct label", () => {
     const mockOnClick = jest.fn();
-    render(<Button label="Click Me" onClick={mockOnClick} />);
+    render(<Button label="按鈕" onClick={mockOnClick} />);
 
-    const button = screen.getByText("Click Me");
+    const button = screen.getByText("按鈕");
     expect(button).toBeInTheDocument();
 
     fireEvent.click(button);
     expect(mockOnClick).toHaveBeenCalledTimes(1);
 });
+
